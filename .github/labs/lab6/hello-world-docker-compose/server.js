51040ec489314e95b6a6a2c13dee0e6761fa1019
MELE MBE Tresor <tresor.melembe@orange.com>
2022-12-27 16:23:32 +0100

    configuration de index.js

--- .github/labs/lab6/hello-world-docker-compose/server.js
+++ .github/labs/lab6/hello-world-docker-compose/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const client = require('./dbClient');
-
+const promClient = require('prom-client')
 const PORT = 8080;
 
 function get_hit_count(callback) {
@@ -20,6 +20,11 @@ app.get('/', (req, res) => {
     res.send('Hello World from Docker! I have been seen ' + count + ' times');
   })
 });
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', promClient.register.contentType)
+  res.end(promClient.register.metrics())
+})
+
 
 app.listen(PORT);
 console.log(`Running on http://localhost:${PORT}`);
